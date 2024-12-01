24bae4898b3eab502b74ba60b89a9f15db2add59
Sal Nasir <salnasir@Sals-MacBook-Pro.local>
2024-11-30 19:57:51 -0500

    add/delete boids, clean code

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,22 +1,19 @@
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
     
     public bool flock_centering, velocity_matching, collision_avoidance, wandering, leave_trail;
-    Boid test_boid;
-    Vector3 test_position;
     List<Boid> boids;
-    const int num_boids = 20;
+    public int num_boids = 20;
     const float dt = 0.005f;
     public float speed_multiplier = 1;
+    const int MAX_BOIDS = 100;
 
     const float west_wall = -52.0f, east_wall = 52.0f, north_wall = 29.0f, south_wall = -29.0f;
     const float min_speed = 0.15f, max_speed = 1.5f;
     const float flock_radius = 2.0f, collision_radius = 1.5f, velocity_matching_radius = 1.2f;
-    // Start is called before the first frame update
     
     //quick and dirty to keep these public for scatter function to see, for initial random conditions
     float max_vel = 1.0f, min_vel = -1.0f;
@@ -25,11 +22,7 @@ public class Main : MonoBehaviour
         
     void Start()
     {
-        // boids = new Boid[num_boids];
-        // test_boid = new Boid(new Vector3(0,0,0), new Vector3(0.01f,0,0.01f));
-        // test_position = new Vector3(0,0,0);
-        // boids[0] = test_boid;
-
+        num_boids = 20;
         boids = new List<Boid>(num_boids);
         for (int i = 0; i < num_boids; i++) {
             Vector3 rand_velocity = new Vector3(random_from(min_vel, max_vel), 0, random_from(min_vel, max_vel));
@@ -38,32 +31,37 @@ public class Main : MonoBehaviour
             boids.Insert(i, new Boid(rand_pos, rand_velocity));
         }
     }
-
-     void OnDrawGizmosSelected()
-    {
-        // Draw a yellow sphere at the transform's position
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(new Vector3(0,0,0), 0.1f);
-        Gizmos.DrawSphere(new Vector3(-1,0,0.75f), 0.1f);
-        Gizmos.DrawSphere(new Vector3(1,0,0.75f), 0.1f);
-        
-        
-    }
+    
     // Update is called once per frame
     void Update()
     {
+        //scatter on spacebar press
         if (Input.GetKeyDown(KeyCode.Space)) {
             scatter(boids);
         }
-        //calculate forces
-        foreach (Boid boid in boids) {
-            //wander force 
-            if (wandering) {
-                Vector3 wander_force = new Vector3(random_from(-1, 1), 0, random_from(-1, 1));
-                boid.set_wander_force(wander_force);
-            } else {
-                boid.set_wander_force(new Vector3(0,0,0));
+        
+        //user wants less boids -> delete the game object, remove the boid from the list
+        if (num_boids < boids.Count && num_boids >= 0) {
+            int bc = boids.Count;
+            for (int i = 0; i < bc - num_boids; i++) {
+                Boid b = boids[0];
+                b.get_game_object().GetComponent<Renderer>().material.color = Color.black;
+                Destroy(b.get_game_object());
+                boids.Remove(b);
             }
+        } else if (num_boids > boids.Count && num_boids <= MAX_BOIDS) {     //user wants more boids -> generate more, up to max number
+            int bc = boids.Count;
+            for (int i = 0; i < num_boids - bc; i++) {
+                Vector3 rand_velocity = new Vector3(random_from(min_vel, max_vel), 0, random_from(min_vel, max_vel));
+                Vector3 rand_pos = new Vector3(random_from(min_x_pos, max_x_pos), 0, random_from(min_z_pos, max_z_pos));
+                boids.Add(new Boid(rand_pos, rand_velocity));
+            }
+        }
+
+        //calculate forces/trail
+        foreach (Boid boid in boids) {
+                
+            //generate trail
             if (leave_trail) {
                 boid.enable_trail();
             } else {
@@ -72,6 +70,14 @@ public class Main : MonoBehaviour
 
             //interate, get each individual force.
             //if (not enabled), set force to zero
+            //wander force 
+            if (wandering) {
+                Vector3 wander_force = new Vector3(random_from(-1, 1), 0, random_from(-1, 1));
+                boid.set_wander_force(wander_force);
+            } else {
+                boid.set_wander_force(new Vector3(0,0,0));
+            }
+
             if (flock_centering) {
                 // List<Boid> flock_neighbors = find_flock_neighbors(boids, boid, flock_radius);
                 Vector3 flocking_force = find_flocking_force(boids, boid, flock_radius);
@@ -96,10 +102,9 @@ public class Main : MonoBehaviour
 
         }
         
-        //update velocity and position 
+        //update + clamp velocity and position
         foreach (Boid boid in boids) {
             boid.update_velocity(speed_multiplier * dt);
-            //TODO: clamp velocity here
             boid.set_velocity(clamp(boid.get_velocity(), min_speed, max_speed));
             //update position, bounce bird if needed
             boid.update_position(speed_multiplier * dt);
@@ -111,19 +116,14 @@ public class Main : MonoBehaviour
             if (boid == null) continue;
             boid.draw();
         } 
-
-        // test_boid.update_position(0.01f);
-        // test_boid.draw();
-        // test_boid.update_position(test_position);
-        // test_position += new Vector3(0.0f, 0.0f, 0.001f);
-        // Debug.Log(test_position);
-
     }
 
+    //generate random float from interval [min, max]
     float random_from(float min, float max) {
         return Random.value * (max - min) + min;
     }
 
+    //have boids bounce off walls if hitting them
     void check_boundary(Boid boid) {
         // float error = 0.1f;
         Vector3 position = boid.get_position();
@@ -136,6 +136,7 @@ public class Main : MonoBehaviour
         }
     }
 
+    //clamp speed of void so never too fast or slow
     Vector3 clamp(Vector3 vel, float min_speed, float max_speed) {
         float magnitude = Vector3.Magnitude(vel);
         if (magnitude >= min_speed && magnitude <= max_speed) {
@@ -149,7 +150,6 @@ public class Main : MonoBehaviour
     }
 
     Vector3 find_flocking_force(List<Boid> boids, Boid current_boid, float flock_radius) {
-        // List<Boid> output = new List<Boid>();
         const float epsilon = 0.01f;
         float weight_total = 0;
         Vector3 numerator = new Vector3(0,0,0);
@@ -197,6 +197,7 @@ public class Main : MonoBehaviour
         return total;
     }
 
+    //scatter all boids to random positions with random velocities
     void scatter(List<Boid> boids) {
         foreach (Boid b in boids) {
             //should i give them a new random_velocity??
